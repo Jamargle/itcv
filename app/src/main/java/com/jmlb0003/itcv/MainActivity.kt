937ffe0bbd0eb3740f770c2513a7f4c0817bb92e
Jose <jmlb0003@gmail.com>
2020-12-13 19:43:22 +0100

    wip

--- app/src/main/java/com/jmlb0003/itcv/MainActivity.kt
+++ app/src/main/java/com/jmlb0003/itcv/MainActivity.kt
@@ -10,6 +10,40 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+
+//    poner CI lo primero de todo
+
+
+//    https://coletiv.com/blog/android-github-actions-setup/
+
+
+//    https://developer.github.com/v3/
+//
+//
+//    https://skydoves.medium.com/android-mvvm-architecture-components-using-github-api-f0ab9c2a67a0
+//
+//    https://youtu.be/TnQUb-ACqWs
+//
+//
+//
+    Pantalla inicial:
+        - Pedir el usuario propio/inicial (en el futuro puede ser hacer login o algo)
+            -> navegar a la pantalla principal
+
+    * Pantalla principal:
+        - detalles del perfil seleccionado
+        - lupa para buscar otro perfil
+            -> buscador que pueda muestrar detalles de perfiles y marcarlos como favoritos
+
+    * Pantalla favoritos:
+        - lista de perfiles favoritos (pantalla con repositorios favoritos quizas...pero eso fuera de MVP)
+            -> navegar a detalles de perfil cuando se seleccione uno
+
+    * Settings
+    * Privacidad...
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
