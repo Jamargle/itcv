2363649aebcf2da83efe6b541364fc53a4947339
Jose Lopez <jose.miguel.lopez.bayona2@ing.com>
2020-12-15 20:36:47 +0100

    First iteration for simple implementation of search logic

--- app/src/main/java/com/jmlb0003/itcv/features/search/SearchFragment.kt
+++ app/src/main/java/com/jmlb0003/itcv/features/search/SearchFragment.kt
@@ -1,21 +1,47 @@
 package com.jmlb0003.itcv.features.search
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.jmlb0003.itcv.R
+import com.jmlb0003.itcv.features.search.adapter.SearchResult
+import com.jmlb0003.itcv.features.search.adapter.SearchResultsAdapter
+import com.jmlb0003.itcv.utils.FragmentActivity.showSoftKeyboard
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_search, container, false)
+    private var adapter: SearchResultsAdapter? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.findViewById<View>(R.id.search_view)?.requestFocus()
+        initSearchView(view)
+        initSearchResultsView(view)
+    }
+
+    private fun initSearchView(rootView: View) {
+        rootView.findViewById<SearchView>(R.id.search_view)?.apply {
+            requireActivity().showSoftKeyboard(this)
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+//                    // TODO to be properly implemented
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    // TODO to be properly implemented
+                    adapter?.setSearchResults(newText?.split("")?.filter { it.isNotBlank() }?.map {
+                        SearchResult(profileName = it, isFavorite = newText.length % 2 == 0)
+                    } ?: emptyList())
+                    return true
+                }
+            })
+        }
+    }
+
+    private fun initSearchResultsView(rootView: View) {
+        rootView.findViewById<RecyclerView>(R.id.search_results_list)?.let { recyclerView ->
+            recyclerView.adapter = SearchResultsAdapter().also { adapter = it }
+        }
     }
 }
---
+++ app/src/main/java/com/jmlb0003/itcv/features/search/adapter/SearchResultsAdapter.kt
@@ -0,0 +1,21 @@
+package com.jmlb0003.itcv.features.search.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.jmlb0003.itcv.R
+
+class SearchResultsAdapter : ListAdapter<SearchResult, SearchResultViewHolder>(SearchResultDiffCallback) {
+
+    fun setSearchResults(results: List<SearchResult>) {
+        submitList(results)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = SearchResultViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_list_search, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: SearchResultViewHolder, position: Int) {
+        holder.bindView(getItem(position))
+    }
+}
