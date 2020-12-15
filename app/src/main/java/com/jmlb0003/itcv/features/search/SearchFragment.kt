5b7d279e2cc9fd49d873efaa855efbe4ccbad7ce
Jose Lopez <jose.miguel.lopez.bayona2@ing.com>
2020-12-15 18:52:56 +0100

    Add search screen

---
+++ app/src/main/java/com/jmlb0003/itcv/features/search/SearchFragment.kt
@@ -0,0 +1,21 @@
+package com.jmlb0003.itcv.features.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.jmlb0003.itcv.R
+
+class SearchFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_search, container, false)
+
+    companion object {
+        fun newInstance(): SearchFragment = SearchFragment()
+    }
+}
