3533fef1b9af9626052f1c34b160ebf3a857d3f2
Jose Lopez <jmlb0003@gmail.com>
2021-01-01 17:38:18 +0100

    Remove comments and more not needed stuff from MainActivity

--- app/src/main/java/com/jmlb0003/itcv/MainActivity.kt
+++ app/src/main/java/com/jmlb0003/itcv/MainActivity.kt
@@ -14,39 +14,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-
-//    poner CI lo primero de todo
-
-
-//    https://coletiv.com/blog/android-github-actions-setup/
-
-
-//    https://developer.github.com/v3/
-//
-//
-//    https://skydoves.medium.com/android-mvvm-architecture-components-using-github-api-f0ab9c2a67a0
-//
-//    https://youtu.be/TnQUb-ACqWs
-//
-//
-//
-    Pantalla inicial:
-        - Pedir el usuario propio/inicial (en el futuro puede ser hacer login o algo)
-            -> navegar a la pantalla principal
-
-    * Pantalla principal:
-        - detalles del perfil seleccionado
-        - lupa para buscar otro perfil
-            -> buscador que pueda muestrar detalles de perfiles y marcarlos como favoritos
-
-    * Pantalla favoritos:
-        - lista de perfiles favoritos (pantalla con repositorios favoritos quizas...pero eso fuera de MVP)
-            -> navegar a detalles de perfil cuando se seleccione uno
-
-    * Settings
-    * Privacidad...
-
-
     private lateinit var toolbarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
