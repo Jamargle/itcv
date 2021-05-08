0f4d2ad1bc64a7acdbdbf5b94cd1776bdf5d2df6
Jose Lopez <jmlb0003@gmail.com>
2021-05-08 21:39:28 +0200

    Add use case to get the topics for the given user

---
+++ app/src/main/java/com/jmlb0003/itcv/domain/usecases/GetUserTopicsUseCase.kt
@@ -0,0 +1,40 @@
+package com.jmlb0003.itcv.domain.usecases
+
+import com.jmlb0003.itcv.core.Either
+import com.jmlb0003.itcv.core.exception.Failure
+import com.jmlb0003.itcv.core.interactor.UseCase
+import com.jmlb0003.itcv.domain.model.Repo
+import com.jmlb0003.itcv.domain.model.Topic
+import com.jmlb0003.itcv.domain.repositories.ReposRepository
+import com.jmlb0003.itcv.domain.repositories.TopicsRepository
+
+class GetUserTopicsUseCase(
+    private val reposRepository: ReposRepository,
+    private val topicsRepository: TopicsRepository
+) : UseCase<List<Topic>, GetUserTopicsUseCase.Input>() {
+
+    override suspend fun run(params: Input): Either<Failure, List<Topic>> {
+        val reposOwner = params.username
+        return when (val userRepositories = reposRepository.getUserRepositories(reposOwner)) {
+            is Either.Left -> Either.Left(userRepositories.leftValue)
+            is Either.Right -> Either.Right(getAllTopics(reposOwner, userRepositories.rightValue))
+        }
+    }
+
+    private fun getAllTopics(reposOwner: String, repos: List<Repo>): List<Topic> {
+        val allTopics = mutableListOf<Topic>()
+        repos.forEach { repo ->
+            val repoTopics = when (val topics = topicsRepository.getRepositoryTopics(repo.name, reposOwner)) {
+                is Either.Left -> emptyList()
+                is Either.Right -> topics.rightValue
+            }
+
+            allTopics.addAll(repoTopics)
+        }
+        return allTopics.distinct()
+    }
+
+    data class Input(
+        val username: String
+    )
+}
