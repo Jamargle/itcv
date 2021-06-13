69632b710116d22eae9d8a49b369820674d11f0e
Jose Lopez <jmlb0003@gmail.com>
2021-06-13 21:59:16 +0200

    Remove classes to inject dependencies manually

--- app/src/main/java/com/jmlb0003/itcv/CustomApplication.kt
+++ app/src/main/java/com/jmlb0003/itcv/CustomApplication.kt
@@ -1,13 +1,8 @@
 package com.jmlb0003.itcv
 
 import android.app.Application
-import com.jmlb0003.itcv.core.NetworkHandler
-import com.jmlb0003.itcv.core.SharedPreferencesHandler
-import com.jmlb0003.itcv.core.coroutines.DispatchersImp
 import com.jmlb0003.itcv.di.ApplicationComponent
 import com.jmlb0003.itcv.di.DaggerApplicationComponent
-import com.jmlb0003.itcv.di.MainInjector
-import com.jmlb0003.itcv.di.RepositoriesProvider
 
 class CustomApplication : Application() {
 
@@ -16,13 +11,4 @@ class CustomApplication : Application() {
             .factory().create(applicationContext)
     }
 
-    val mainInjector: MainInjector by lazy { createMainInjector() }
-
-    private fun createMainInjector() = MainInjector(
-        provideApplicationContext = { applicationContext },
-        provideNetworkHandler = { NetworkHandler(applicationContext) },
-        provideDispatchers = { DispatchersImp },
-        provideRepositoriesProvider = { RepositoriesProvider(mainInjector) },
-        provideSharedPreferencesHandler = { SharedPreferencesHandler(applicationContext) }
-    )
 }
